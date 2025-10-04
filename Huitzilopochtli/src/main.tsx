64959bb31a11cb5401685ac56eb29e3cbc43af52
Mosiah Andrade <mosiahassuncao@GMAIL.COM>
2025-10-04 23:14:00 +0000

    register public

--- Huitzilopochtli/src/main.tsx
+++ Huitzilopochtli/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { register } from './registerServiceWorker'
+import { register } from '../public/registerServiceWorker.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
